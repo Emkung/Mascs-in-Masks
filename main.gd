2a6640eff512ddadc4ddfbf576e63be35ab19548
HsiaolanKung <hsiaolankung@gmail.com>
2026-06-06 21:12:00 +0800

    phone stuff

--- main.gd
+++ main.gd
@@ -1,10 +1,12 @@
 extends Node2D
 @onready var bg_phone: Sprite2D = $bg_phone
-
+@export var nix_button: TextureButton 
+@export var ion_button: TextureButton
+@export var animation_player: AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("box")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +23,26 @@ func _on_nix_pressed() -> void:
 func _on_ion_pressed() -> void:
 	Dialogic.start("ionTimeline1")
 	bg_phone.hide()
+
+
+func _on_nix_mouse_entered() -> void:
+	animation_player.play("nix")
+	
+	
+
+
+func _on_nix_mouse_exited() -> void:
+	#nix_button.z_index = 0
+	#nix_button.scale = Vector2(0.85,0.85)
+	animation_player.play("nix out")
+	
+
+
+func _on_ion_mouse_entered() -> void:
+	ion_button.z_index = 1
+	ion_button.scale = Vector2(0.9,0.9)
+
+
+func _on_ion_mouse_exited() -> void:
+	ion_button.z_index = 0
+	ion_button.scale = Vector2(0.85,0.85)
